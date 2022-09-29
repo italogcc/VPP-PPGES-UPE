230084dcc56fc773297ab225484f283fb4c6e2a7
italogcc <italogcc@live.com>
2022-09-29 12:39:41 -0300

    Atualização

--- base/_vppGUI-Testes-NaoTerminado.py
+++ base/_vppGUI-Testes-NaoTerminado.py
@@ -9,16 +9,69 @@ class VPP:
         # Escolha do tema da GUI
         sg.theme('DefaultNoMoreNagging')
         # Dados da janela
-        layout = [  [sg.Text('Escolha quais subgrupos estarão ativados e a capacidade de geração solar')],
+
+        coluna_A1 = [
                     [sg.Text('SUBGRUPO A1')],
                     [sg.Radio('Ativado', 'sgrupo_a1_ativacao', key='sgrupo_a1_ativacao')],
                     [sg.Radio('Desativado', 'sgrupo_a1_ativacao', key='sgrupo_a1_ativacao')],
-                    [sg.Text('Geração solar:', size=(6, 0)), sg.Input(size=(6, 0), key=('sgrupo_a1_gdsolar'))],
-                    [sg.Slider(range=(0, 255), default_value=0, orientation='h', size=(12, 20),
+                    [sg.Text('GD-Solar:', size=(8, 0)), sg.Input(size=(6, 0), key=('sgrupo_a1_gdsolar'))],
+                    [sg.Slider(range=(0, 300), default_value=0, orientation='h', size=(12, 20),
                                key='sgrupo_a1_gdsolar_variacao')],
-                    [sg.Button('Carregar configurações'), sg.Button('Rodar o OpenDSS'), sg.Button('Sair do programa')],
-                    [sg.Output(size=(30,20))]
-                ]
+                    [sg.Text('AD-Bateria:', size=(8, 0)), sg.Input(size=(6, 0), key=('sgrupo_a1_adbateria'))],
+                    [sg.Slider(range=(0, 10), default_value=0, orientation='h', size=(12, 20),
+                               key='sgrupo_a1_adbateria_variacao')],
+        ]
+
+        coluna_A2 = [
+                    [sg.Text('SUBGRUPO A2')],
+                    [sg.Radio('Ativado', 'sgrupo_a2_ativacao', key='sgrupo_a2_ativacao')],
+                    [sg.Radio('Desativado', 'sgrupo_a2_ativacao', key='sgrupo_a2_ativacao')],
+                    [sg.Text('GD-Solar:', size=(8, 0)), sg.Input(size=(6, 0), key=('sgrupo_a2_gdsolar'))],
+                    [sg.Slider(range=(0, 300), default_value=0, orientation='h', size=(12, 20),
+                               key='sgrupo_a2_gdsolar_variacao')],
+                    [sg.Text('AD-Bateria:', size=(8, 0)), sg.Input(size=(6, 0), key=('sgrupo_a2_adbateria'))],
+                    [sg.Slider(range=(0, 10), default_value=0, orientation='h', size=(12, 20),
+                               key='sgrupo_a2_adbateria_variacao')],
+        ]
+
+        coluna_A3 = [
+                    [sg.Text('SUBGRUPO A3')],
+                    [sg.Radio('Ativado', 'sgrupo_a3_ativacao', key='sgrupo_a3_ativacao')],
+                    [sg.Radio('Desativado', 'sgrupo_a3_ativacao', key='sgrupo_a3_ativacao')],
+                    [sg.Text('GD-Solar:', size=(8, 0)), sg.Input(size=(6, 0), key=('sgrupo_a3_gdsolar'))],
+                    [sg.Slider(range=(0, 300), default_value=0, orientation='h', size=(12, 20),
+                               key='sgrupo_a3_gdsolar_variacao')],
+                    [sg.Text('AD-Bateria:', size=(8, 0)), sg.Input(size=(6, 0), key=('sgrupo_a3_adbateria'))],
+                    [sg.Slider(range=(0, 10), default_value=0, orientation='h', size=(12, 20),
+                               key='sgrupo_a3_adbateria_variacao')],
+        ]
+        
+        coluna_A4 = [
+                    [sg.Text('SUBGRUPO A4')],
+                    [sg.Radio('Ativado', 'sgrupo_a4_ativacao', key='sgrupo_a4_ativacao')],
+                    [sg.Radio('Desativado', 'sgrupo_a4_ativacao', key='sgrupo_a4_ativacao')],
+                    [sg.Text('GD-Solar', size=(8, 0)), sg.Input(size=(6, 0), key=('sgrupo_a4_gdsolar'))],
+                    [sg.Slider(range=(0, 300), default_value=0, orientation='h', size=(12, 20),
+                               key='sgrupo_a4_gdsolar_variacao')],
+                    [sg.Text('AD-Bateria:', size=(8, 0)), sg.Input(size=(6, 0), key=('sgrupo_a4_adbateria'))],
+                    [sg.Slider(range=(0, 10), default_value=0, orientation='h', size=(12, 20),
+                               key='sgrupo_a4_adbateria_variacao')],
+        ]
+
+        layout = [
+            [sg.Text('Escolha quais subgrupos estarão ativados e a capacidade de GD e AD de cada um:')],
+            [
+                sg.Column(coluna_A1),
+                sg.VSeperator(),
+                sg.Column(coluna_A2),
+                sg.VSeperator(),
+                sg.Column(coluna_A3),
+                sg.VSeperator(),
+                sg.Column(coluna_A4),
+            ],
+            [sg.Button('Carregar configurações'), sg.Button('Rodar o OpenDSS'), sg.Button('Sair do programa')],
+            [sg.Output(size=(80, 15))]
+        ]
 
         # Cria a janela
         window = sg.Window('VPP', layout)
@@ -32,10 +85,51 @@ class VPP:
             sgrupo_a1_ativacao = values['sgrupo_a1_ativacao']
             sgrupo_a1_gdsolar = values['sgrupo_a1_gdsolar']
             sgrupo_a1_gdsolar_variacao = values['sgrupo_a1_gdsolar_variacao']
+            sgrupo_a1_adbateria = values['sgrupo_a1_adbateria']
+            sgrupo_a1_adbateria_variacao = values['sgrupo_a1_adbateria_variacao']
+
+            sgrupo_a2_ativacao = values['sgrupo_a2_ativacao']
+            sgrupo_a2_gdsolar = values['sgrupo_a2_gdsolar']
+            sgrupo_a2_gdsolar_variacao = values['sgrupo_a2_gdsolar_variacao']
+            sgrupo_a2_adbateria = values['sgrupo_a2_adbateria']
+            sgrupo_a2_adbateria_variacao = values['sgrupo_a2_adbateria_variacao']
+
+            sgrupo_a3_ativacao = values['sgrupo_a3_ativacao']
+            sgrupo_a3_gdsolar = values['sgrupo_a3_gdsolar']
+            sgrupo_a3_gdsolar_variacao = values['sgrupo_a3_gdsolar_variacao']
+            sgrupo_a3_adbateria = values['sgrupo_a3_adbateria']
+            sgrupo_a3_adbateria_variacao = values['sgrupo_a3_adbateria_variacao']
+
+            sgrupo_a4_ativacao = values['sgrupo_a4_ativacao']
+            sgrupo_a4_gdsolar = values['sgrupo_a4_gdsolar']
+            sgrupo_a4_gdsolar_variacao = values['sgrupo_a4_gdsolar_variacao']
+            sgrupo_a4_adbateria = values['sgrupo_a4_adbateria']
+            sgrupo_a4_adbateria_variacao = values['sgrupo_a4_adbateria_variacao']
+
             # print(f'nomeDaVar: {Valor}')
             print(f'sgrupo_a1_ativacao: {sgrupo_a1_ativacao}')
             print(f'sgrupo_a1_gdsolar: {sgrupo_a1_gdsolar}')
             print(f'sgrupo_a1_gdsolar_variacao: {sgrupo_a1_gdsolar_variacao}')
+            print(f'sgrupo_a1_adbateria: {sgrupo_a1_adbateria}')
+            print(f'sgrupo_a1_adbateria_variacao: {sgrupo_a1_adbateria_variacao}')
+
+            print(f'sgrupo_a2_ativacao: {sgrupo_a2_ativacao}')
+            print(f'sgrupo_a2_gdsolar: {sgrupo_a2_gdsolar}')
+            print(f'sgrupo_a2_gdsolar_variacao: {sgrupo_a2_gdsolar_variacao}')
+            print(f'sgrupo_a2_adbateria: {sgrupo_a2_adbateria}')
+            print(f'sgrupo_a2_adbateria_variacao: {sgrupo_a2_adbateria_variacao}')
+
+            print(f'sgrupo_a3_ativacao: {sgrupo_a3_ativacao}')
+            print(f'sgrupo_a3_gdsolar: {sgrupo_a3_gdsolar}')
+            print(f'sgrupo_a3_gdsolar_variacao: {sgrupo_a3_gdsolar_variacao}')
+            print(f'sgrupo_a3_adbateria: {sgrupo_a3_adbateria}')
+            print(f'sgrupo_a3_adbateria_variacao: {sgrupo_a3_adbateria_variacao}')
+
+            print(f'sgrupo_a4_ativacao: {sgrupo_a4_ativacao}')
+            print(f'sgrupo_a4_gdsolar: {sgrupo_a4_gdsolar}')
+            print(f'sgrupo_a4_gdsolar_variacao: {sgrupo_a4_gdsolar_variacao}')
+            print(f'sgrupo_a4_adbateria: {sgrupo_a4_adbateria}')
+            print(f'sgrupo_a4_adbateria_variacao: {sgrupo_a4_adbateria_variacao}')
 
             # Evento para fechamento da janela do programa
             if event == sg.WIN_CLOSED or event == 'Sair do programa':
@@ -85,7 +179,7 @@ dss.text("clear")
 
 # Caminho do arquivo DSS
 # dss_arquivo = str(r"D:\projetos\opendss\arquivo.dss").strip()
-dss_arquivo = str(r"D:\Projetos\opendss\VPP-PPGES-UPE\Cenarios05\vppbase.dss").strip()
+dss_arquivo = str(r"D:\Projetos\opendss\VPP-PPGES-UPE\vppbase-cenario08.dss").strip()
 
 # Compila o arquivo DSS
 dss.text("compile {}".format(dss_arquivo))
